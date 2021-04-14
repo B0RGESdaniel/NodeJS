b6b7a918516eeebad84496bc06d53c7f9a5f1424
Daniel Borges <dlopesborges@gmail.com>
2021-04-14 12:36:44 -0300

    Updating HTTP methods

--- src/index.js
+++ src/index.js
@@ -1,34 +1,32 @@
 const express = require('express');
+const { uuid } = require('uuidv4')
 
 const app = express();
 // aplicação criada :)
 
 app.use(express.json());
 
+const projects = [];
+
 // get recebe dois parâmetros(rota, função que recebe os parametros(request, response))
 app.get('/projects', (request, response) => {
-    const { title, owner } = request.query;
-    // desestruturando a resposta do query
-    console.log(title);
-    console.log(owner);
+    // const { title, owner } = request.query;
+    // // desestruturando a resposta do query
+    // console.log(title);
+    // console.log(owner);
 
-   return response.json([
-       'Projeto 1',
-       'Projeto 2',
-   ]);
+   return response.json(projects);
 })
 
 app.post('/projects', (request, response) => {
     // exemplo de request body
     const { title, owner } = request.body;
 
-    console.log(`este é o ${title} de ${owner}`);
+    const project = { id: uuid(), title: title, owner };
+
+    projects.push(project);
     
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3',
-    ]);
+    return response.json(project);
 })
 
 app.put('/projects/:id', (request, response) => {
